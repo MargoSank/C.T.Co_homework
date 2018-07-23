45f1c285126bed95f911fcb7dea67c8fd9cf54f1
MargoSank <36936288+MargoSank@users.noreply.github.com>
2018-07-23 19:03:31 +0200

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,12 +1,19 @@
-
+import java.util.Scanner;
 public class Task2 {
 
 	public static void main(String[] args) {
-		
-		int input = 9; //
-		
+		int input;
 		int count;
 		int level;
+		Scanner user = new Scanner(System.in);
+		System.out.print("Enter the number:");
+		if (user.hasNextInt())
+			input = user.nextInt();
+		else {
+			System.out.println("input error");
+			user.close();
+			return;
+		}
 		if(input%2==0){
 			count = 2;
 			level=input/2;
@@ -24,7 +31,6 @@ public class Task2 {
 			count=count+2;
 			System.out.println();
 		}
+	user.close();
 	}
 }
-
-
